3982a714eb575da4cdbecbd4a7bc117385b5e920
menhaj <menhaj007@yahoo.com>
2021-04-06 10:40:43 -0400

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+function saturdayFun(value = "roller-skate") {
+    return `This Saturday, I want to ${value}!`;
+}
+function mondayWork(value = "go to the office") {
+    return `This Monday, I will ${value}.`
+}
+
+function wrapAdjective(vFlair = "*") {
+    
+    return function(value = "special") {
+        return `You are ${vFlair}${value}${vFlair}!`;
+    }
+}
+
+
+console.log(wrapAdjective("*")("a dedicated programmer"));
